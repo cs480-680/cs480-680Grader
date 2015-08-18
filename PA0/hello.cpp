c26306cb2afec156db7a8026fea06898498b62a3
Wesley Kepke <wesleykepke@nevada.unr.edu>
2015-08-18 16:12:24 -0700

    Added new line!

--- PA0/hello.cpp
+++ PA0/hello.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main(void) {
 	cout << "Hello World!" << endl;
-	cout << "This is not a graphics program.." << endl; 
+	cout << "This is not a graphics program." << endl; 
 	return 0;
 }
